bfcb9971171b1318918b19014f8336f2d9832ecc
Mohamed Shadab <mohamedshadab78@gmail.com>
2020-09-08 22:03:12 +0400

    addresses actionables mentioned in issue #57 (#63)

---
+++ IT/214_database_management_system/p_m_jat/2019/lab_sessions/labs_ans_source_1/throughput_plot.py
@@ -0,0 +1,59 @@
+import matplotlib.pyplot as plt
+import math
+
+fp = open("parallel_vector_add.txt", "r")
+f = open("serial_vector_add.txt", "r")
+
+s = []
+p = []
+ser = []
+
+for i, j in zip(f, fp):
+	t = i.split()
+	tp = j.split()
+	#print(t[0])
+	s.append(math.log(long(t[0]), 2))
+	if(tp[1] == "inf"):
+		tmp = 0
+		sup.append(tmp)
+	elif(t[1] == "inf"):
+		tmp = float("inf")
+		sup.append(tmp)
+	else:
+		tmp = float(t[1])
+		ser.append(tmp)
+#		tmp = float(tp[1])
+#		p.append(tmp)
+
+plt.plot(s[0:16], ser[0:16], marker='^', label="serial")
+#plt.ylabel('Speedup')
+#plt.xlabel('Size (Bytes)')
+
+fp = open("parallel_square.txt", "r")
+f = open("serial_square.txt", "r")
+
+s = []
+sup = []
+
+for i, j in zip(f, fp):
+        t = i.split()
+        tp = j.split()
+        #print(t[0])
+        s.append(math.log(long(t[0]), 2))
+	#print(s)
+        if(tp[1] == "inf"):
+                tmp = 0
+                sup.append(tmp)
+        elif(t[1] == "inf"):
+                tmp = float("inf")
+                sup.append(tmp)
+        else:
+                tmp = float(t[1])
+                sup.append(tmp)
+
+plt.plot(s[0:16], sup[0:16], marker='o', label="square")
+plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3, ncol=2, mode="expand", borderaxespad=0.)
+plt.ylabel('throughput')
+plt.xlabel('Size (Bytes)')
+plt.savefig('plot_tp.png')
+plt.show()
